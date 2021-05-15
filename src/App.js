7c6281855571bcd8cb5aafd1e5b061a54ec6bc6b
klamb95 <klamb1995@gmail.com>
2021-05-15 12:00:42 +0100

    added image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import CharacterContainer from './containers/CharacterContainer';
 import './App.css';
+import BreakingBad from './BreakingBad.jpg';
 
 function App() {
   return (
     <div className="App">
      <h1>Breaking Bad Characters</h1>
      <CharacterContainer/>
+     <img src={BreakingBad} className='BBimg' alt="logo"/>
     </div>
   );
 }
